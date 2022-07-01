ee86159b10409060b0da99cf83b4c68e510f8a2b
harshsingh9898 <73526355+harshsingh9898@users.noreply.github.com>
2022-07-01 19:00:40 +0530

    Add files via upload

---
+++ Address_Book_sys.sql
@@ -0,0 +1,59 @@
+/*UC1 - Creating AddressBookService DataBase*/
+
+create database AddressBookService
+
+/*UC2 - Creating table */
+
+create table AddressBook(
+firstName varchar(255) not null,
+lastname varchar(255) not null,
+Address varchar(255) not null,
+City varchar(255) not null,
+State varchar(255) not null,
+Zip int not null,
+PhoneNumber bigint,
+email varchar(255)
+)
+
+/*UC3 - Inserting data into AddressBook table*/
+insert into AddressBook (firstName,lastname,Address,City,State,Zip,PhoneNumber,email) values('Tushar','Vidwans','Malkapur','Akola','Maharashtra',444002,9096486283,'tusharvidwans99@gmail.com')
+insert into AddressBook values
+('Saran','Yellanki','Kurmannapalem','Vizag','Andhra Pradesh',530046,9618058892,'saran.yellanki12@gmail.com'),
+('Shubham','Verma','Gachibowli','Hyderabad','Telangana',520017,9775638892,'shubham.verma@yahoo.co.in'),
+('Madhukar','Tatiraju','Gurudwar','Vizag','Andhra Pradesh',530032,9347665123,'mad.t@gmail.com'),
+('Basila','Noushad','Mattancherry','Kochi','Kerala',510033,9688920910,'basila.noushad@yahoo.com'),
+('Manasa','Pochiraju','Steel Plant','Vizag','Andhra Pradesh',530032,9848553322,'manasa.p@yahoo.com'),
+('Sai','Chandu','Chanda Nagar','Hyderabad','Telangana',540081,9909339211,'saichandu998@gmail.com')
+
+/*UC4 - Editing contact by their name*/
+update AddressBook
+set firstName='Sumit'
+where firstName='Shubham'
+
+select * from AddressBook
+
+/*UC5 - Deleting contact person by their name*/
+
+delete AddressBook
+where firstName='Basila' and lastname='Noushad'
+
+/*UC6 - Retrieving person contact base on City or State*/
+select * from AddressBook
+where City='Vizag'
+
+select * from AddressBook
+where State='Telangana'
+
+
+/*UC7 - Getting size of the AddressBook by city or state*/
+
+select City, count(City) from AddressBook
+Group BY City
+
+select State, count(State) from AddressBook
+Group by State
+
+/*UC8 - Retrieving Person contact in alphabetical order by name for given city*/
+select * from AddressBook 
+where City='Vizag'
+order by firstName asc
